11ed812b02d2e95561d871b7ff1a62bfe1f6171d
Andrew Roberts <andrew.jacob.roberts@gmail.com>
2020-06-02 15:27:32 -0400

    Modify: Navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -9,15 +9,14 @@ export function Navbar() {
     <nav className="bg-white">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex h-16">
-          <div className="flex">
-            <div className="flex items-center mr-6">
+          <div className="flex min-h-full">
+            <div className="flex items-center flex-wrap mr-6">
               <Link to="/" className="flex hover:z-50 focus:z-50">
                 <div className="flex items-center">
                   <SvgLogo className="h-12" />
                 </div>
               </Link>
             </div>
-
             <PartialNavLink to={"/archive"}>Archive</PartialNavLink>
             <PartialNavLink to={"/donate"}>Donate</PartialNavLink>
             <PartialNavLink to={"/about"}>About</PartialNavLink>
@@ -33,10 +32,10 @@ export function Navbar() {
 function isPartiallyActive({ isPartiallyCurrent }) {
   return isPartiallyCurrent
     ? {
-        className: `ml-8 inline-flex items-center px-1 pt-1 border-b-2 border-indigo-500 text-sm font-medium leading-5 text-gray-900 focus:outline-none focus:border-indigo-700 transition duration-150 ease-in-out"`,
+        className: `ml-2 md:ml-8 inline-flex items-center px-1 pt-1 border-b-2 border-indigo-500 text-sm font-medium leading-5 text-gray-900 focus:outline-none focus:border-indigo-700 transition duration-150 ease-in-out"`,
       }
     : {
-        className: `ml-8 inline-flex items-center px-1 pt-1 border-b-2 border-transparent text-sm font-medium leading-5 text-gray-500 hover:text-gray-700 hover:border-gray-300 focus:outline-none focus:text-gray-700 focus:border-gray-300 transition duration-150 ease-in-out`,
+        className: `ml-2 md:ml-8 inline-flex items-center px-1 pt-1 border-b-2 border-transparent text-sm font-medium leading-5 text-gray-500 hover:text-gray-700 hover:border-gray-300 focus:outline-none focus:text-gray-700 focus:border-gray-300 transition duration-150 ease-in-out`,
       };
 }
 
--- src/pages/about/index.js
+++ src/pages/about/index.js
@@ -16,10 +16,16 @@ export default function AboutPage() {
       <main>
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
           <div class="px-4 py-8 sm:px-0">
-            <p>
-              We will not be silenced. Never forget the protests that George
-              Floyd's death and 400 years of oppression set off.
+            <p className="font-semibold">
+              "Darkness cannot drive out darkness; only light can do that. Hate
+              cannot drive out hate; only love can do that." - Martin Luther
+              King Jr.
             </p>
+            <p className="mt-6">
+              The purpose of this site is to remember the George Floyd and anti
+              police brutality protests.
+            </p>
+            <p className="mt-6">We will not be silenced. We are done dying.</p>
           </div>
         </div>
       </main>
