67e26c4de51a0843c605de9ae1655e4779750128
Andrew Roberts <andrew.jacob.roberts@gmail.com>
2020-06-03 21:41:47 -0400

    Modify: ProtestMap, wrap Map in window check for gatsby

--- src/components/ProtestMap/index.js
+++ src/components/ProtestMap/index.js
@@ -20,42 +20,45 @@ const CENTER_OF_UNITED_STATES = [39.828, -98.579];
 
 export function ProtestMap() {
   const { nodes } = useArchive();
-  return (
-    <Map center={CENTER_OF_UNITED_STATES} zoom={4}>
-      <TileLayer
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-      />
-      {nodes.map((archiveItem, index) => {
-        return (
-          <Marker
-            position={[
-              archiveItem.frontmatter.lat,
-              archiveItem.frontmatter.long,
-            ]}
-          >
-            <Popup>
-              <h3>{`${new Date(
-                archiveItem.frontmatter.date
-              ).toLocaleString()} @ ${archiveItem.frontmatter.location}`}</h3>
-              <div className="flex">
-                <a
-                  href={buildTwitterSearchUrl({
-                    location: archiveItem.frontmatter.location,
-                    date: archiveItem.frontmatter.date,
-                  })}
-                  className="mr-2"
-                >
-                  <SvgTwitterIcon height={"32px"} />
-                </a>
-                <Link to={archiveItem.frontmatter.slug}>
-                  <SvgLogo height={"32px"} />
-                </Link>
-              </div>
-            </Popup>
-          </Marker>
-        );
-      })}
-    </Map>
-  );
+  if (typeof window !== "undefined") {
+    return (
+      <Map center={CENTER_OF_UNITED_STATES} zoom={4}>
+        <TileLayer
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+        />
+        {nodes.map((archiveItem, index) => {
+          return (
+            <Marker
+              position={[
+                archiveItem.frontmatter.lat,
+                archiveItem.frontmatter.long,
+              ]}
+            >
+              <Popup>
+                <h3>{`${new Date(
+                  archiveItem.frontmatter.date
+                ).toLocaleString()} @ ${archiveItem.frontmatter.location}`}</h3>
+                <div className="flex">
+                  <a
+                    href={buildTwitterSearchUrl({
+                      location: archiveItem.frontmatter.location,
+                      date: archiveItem.frontmatter.date,
+                    })}
+                    className="mr-2"
+                  >
+                    <SvgTwitterIcon height={"32px"} />
+                  </a>
+                  <Link to={archiveItem.frontmatter.slug}>
+                    <SvgLogo height={"32px"} />
+                  </Link>
+                </div>
+              </Popup>
+            </Marker>
+          );
+        })}
+      </Map>
+    );
+  }
+  return null;
 }
